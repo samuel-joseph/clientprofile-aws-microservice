11bb2eeb4f62f1200a1e6316043463a425e62506
arcanlte <pinangayjoel@gmail.com>
2022-01-18 16:49:10 -0500

    To reset go here

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const profileRoutes = require("./routes/profile-routes");
 app.use("/profile", profileRoutes);
 
 const portfolioRoutes = require("./routes/portfolio-routes");
+const res = require("express/lib/response");
 app.use("/portfolio", portfolioRoutes);
 
 db.sequelize.sync().then(() => {
