8d045efc95d6deaaf88eed4e8569a0622498ba36
arcanlte <pinangayjoel@gmail.com>
2022-01-18 17:23:19 -0500

    To deploy in heroku

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,7 @@
-// store the configuration secrets of the PG DB here
 module.exports = {
-  HOST: process.env.HOST, // your endpoint
-  USER: process.env.USER, // your username
-  PASSWORD: process.env.PASSWORD, // your password
-  DB: process.env.DB, // your db name
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
   dialect: "postgres",
 };
--- models/index.js
+++ models/index.js
@@ -6,20 +6,9 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
+
 const dbConfig = require("../config/dbConfig");
 const db = {};
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
 
 let sequelize = null;
 
@@ -34,7 +23,6 @@ if (process && process.env.DATABASE_URL) {
   });
 } else {
   sequelize = new Sequelize({
-    // use imported configurations from dbConfig
     database: dbConfig.DB,
     username: dbConfig.USER,
     password: dbConfig.PASSWORD,
