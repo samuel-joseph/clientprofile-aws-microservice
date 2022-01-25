fd2bff9b851263771ecf5be0301720c146d9818c
arcanlte <pinangayjoel@gmail.com>
2022-01-25 12:25:57 -0500

    Changes made

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -8,7 +8,7 @@ router.post("/", (req, res) => {
       customer_id: req.body.customer_id,
     },
   });
-
+  console.log(clientCheck);
   if (clientCheck.length > 0) {
     res.send("Customer id already exist!");
   } else {
