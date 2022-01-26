89a110cc8ff8a3ddf1e5a51982392b6a2d8e13b4
arcanlte <pinangayjoel@gmail.com>
2022-01-26 09:19:00 -0500

    Test

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -11,45 +11,68 @@ const db = require("../models");
 //   }).then((chosenProfile) => res.send(chosenProfile));
 // });
 
-const clientIdSearch = async () => {
-  await db.Client.findOne({
-    where: { customer_id: req.params.customer_id },
-  });
-};
+// const funCall = async () => {
+//   await db.Client.findOne({
+//     where: { customer_id: req.params.customer_id },
+//   });
+// };
 
 router.get("/:customer_id", (req, res) => {
-  let clientId = clientIdSearch.dataValues.id;
+  const funCall = async () => {
+    await db.Client.findOne({
+      where: { customer_id: req.params.customer_id },
+    });
+    let clientId = clientIdSearch.dataValues.id;
 
-  db.ClientProfile.findAll({
-    where: {
-      ClientId: clientId,
-    },
-    include: [db.Client],
-  }).then((chosenProfile) => res.send(chosenProfile));
+    db.ClientProfile.findAll({
+      where: {
+        ClientId: clientId,
+      },
+      include: [db.Client],
+    }).then((chosenProfile) => res.send(chosenProfile));
+  };
+
+  funCall();
 });
 
 router.post("/", (req, res) => {
-  db.ClientProfile.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    birthdate: req.body.birthdate,
-    age: req.body.age,
-    ClientId: req.body.ClientId,
-  }).then((postProfile) => res.send(postProfile));
-});
+  const funCall = async () => {
+    await db.Client.findOne({
+      where: { customer_id: req.body.customer_id },
+    });
+    let clientId = clientIdSearch.dataValues.id;
 
-router.put("/:ClientId", (req, res) => {
-  db.ClientProfile.update(
-    {
+    db.ClientProfile.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       birthdate: req.body.birthdate,
       age: req.body.age,
-    },
-    { where: { ClientId: req.params.ClientId } }
-  ).then((updated) => res.send(updated));
+      ClientId: clientId,
+    }).then((postProfile) => res.send(postProfile));
+  };
+
+  funCall();
+});
+
+router.put("/:customer_id", (req, res) => {
+  const funCall = async () => {
+    await db.Client.findOne({
+      where: { customer_id: req.params.customer_id },
+    });
+    let clientId = clientIdSearch.dataValues.id;
+    db.ClientProfile.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        birthdate: req.body.birthdate,
+        age: req.body.age,
+      },
+      { where: { ClientId: clientId } }
+    ).then((updated) => res.send(updated));
+  };
+  funCall();
 });
 
 module.exports = router;
