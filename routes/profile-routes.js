b29dae0901fe226f76292250cf72e220aa5441a1
arcanlte <pinangayjoel@gmail.com>
2022-01-26 09:07:33 -0500

    Test

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,13 +2,32 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get("/:id", (req, res) => {
-  db.ClientProfile.findAll({
-    where: {
-      ClientId: req.params.id,
-    },
-    include: [db.Client],
-  }).then((chosenProfile) => res.send(chosenProfile));
+// router.get("/:id", (req, res) => {
+//   db.ClientProfile.findAll({
+//     where: {
+//       ClientId: req.params.id,
+//     },
+//     include: [db.Client],
+//   }).then((chosenProfile) => res.send(chosenProfile));
+// });
+
+let clientIdSearch = await db.Client.findOne({
+  where: { customer_id: req.params.customer_id },
+});
+
+router.get("/:customer_id", (req, res) => {
+  const funCall = async () => {
+    let clientId = clientIdSearch.dataValues.id;
+
+    db.ClientProfile.findAll({
+      where: {
+        ClientId: clientId,
+      },
+      include: [db.Client],
+    }).then((chosenProfile) => res.send(chosenProfile));
+  };
+
+  funCall();
 });
 
 router.post("/", (req, res) => {
